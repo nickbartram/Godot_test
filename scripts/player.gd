78747cea204cba2c6516945295d31e29d78377b5
nickbartram <nickbartram25@gmail.com>
2025-07-10 16:05:39 -0400

    Finished rough project

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,44 @@
-extends Area2D
+extends CharacterBody2D
 
 
-# Using a signal, from tutorial
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
 
-func _on_body_entered(body: Node2D) -> void:
-	print("+1 coin!")
-	queue_free()
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Flip direction
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jump")
+
+	
+	# Get the input direction
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
